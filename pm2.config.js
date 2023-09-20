71e87f8826b8c13a979d3724880354f1bc74428a
Troy Dorman <troydorman@gmail.com>
2023-09-19 22:33:35 -0600

    new pm2 start script

--- pm2.config.js
+++ pm2.config.js
@@ -1,17 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: 'pt', // Change this to your app's name
+      name: 'front',
       script: 'npm',
-      args: 'start',
-      cwd: '/var/www/html', // Change this to the absolute path of your project
-      instances: 'max',
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
+      args: 'run dev', // Run Next.js in development mode. Use 'start' for production
+      watch: true,
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: 'development',
       },
+    },
+    {
+      name: 'back',
+      script: 'nodemon server/index.js',
+      watch: true,
+      env: {
+        NODE_ENV: 'development',
+        PORT: 8888, // Assuming your backend runs on port 5000
+      },
+      exec_mode: 'cluster', // if you want to use clustering
+      instances: 'max', // or number of instances
     },
   ],
 }
